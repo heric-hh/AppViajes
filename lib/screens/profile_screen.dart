72c2a5d365a9e2583792d1365c47b68c4b138fa0
heric-hh <erhh.015@gmail.com>
2023-03-13 19:05:09 -0600

    Se añade foto del perfil y nombre, ademas del row de iconos debajo del nombre

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -11,7 +11,22 @@ class ProfileScreen extends StatelessWidget {
     return Stack(
       children: [  
           
-          GradientBack(),
+          //* Gradiente
+          Container(
+            height: 400,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 155, 117, 245),
+                  Color.fromARGB(255, 85, 44, 180),
+                ],
+                begin: FractionalOffset(0.2, 0.0),
+                end: FractionalOffset(1.0, 0.6),
+                stops: [0.0, 0.6],
+                tileMode: TileMode.clamp,
+              ),
+            ),
+          ),
           
           Stack(
             children: [
@@ -21,7 +36,7 @@ class ProfileScreen extends StatelessWidget {
                     padding: EdgeInsets.only(top: 80, left: 30),
                     child: Text(
                       'Perfil',
-                      style: TextStyle(
+                      style: GoogleFonts.lato(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
                         color: Colors.white
@@ -29,15 +44,84 @@ class ProfileScreen extends StatelessWidget {
                     ),
                   ),
 
+                  //* Container del icono
                   Container(
                     padding: EdgeInsets.only(top: 85, left: 230),
                     child: Icon(Icons.ac_unit_sharp, color: Colors.white,),
                   )
-
                 ],
               ),
 
+            //* Row del profile pic y nombre del perfil
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 140, left: 30),
+                    width: 80,
+                    height: 80,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(image: AssetImage('assets/img/jam.jpg'), fit: BoxFit.cover)
+                    ),
+                  ),
 
+                  Container(
+                    margin: EdgeInsets.only(top: 140, left: 20),
+                    child: Text('Filomena Pérez', style: GoogleFonts.lato(fontSize: 18, color: Colors.white70),),
+                  )
+                ],
+              ),
+
+              //* Row de la tira de iconos
+              Container(
+                margin: EdgeInsets.only(top: 250),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(
+                      onPressed: () {},
+                      mini: true,
+                      backgroundColor: Colors.white38,
+                      child: Icon(Icons.charging_station, color: Colors.indigo,),
+                    ),
+
+                    FloatingActionButton(
+                      onPressed: () {},
+                      mini: true,
+                      backgroundColor: Colors.white38,
+                      child: Icon(Icons.memory, color: Colors.indigo,),
+                    ),
+
+                    FloatingActionButton(
+                      onPressed: () {},
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.add, color: Colors.indigo,),
+                    ),
+
+                    FloatingActionButton(
+                      onPressed: () {},
+                      mini: true,
+                      backgroundColor: Colors.white38,
+                      child: Icon(Icons.credit_card, color: Colors.indigo,),
+                    ),
+
+                    FloatingActionButton(
+                      onPressed: () {},
+                      mini: true,
+                      backgroundColor: Colors.white38,
+                      child: Icon(Icons.person, color: Colors.indigo,),
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(
+                child: Stack(
+                  children: [
+                    
+                  ]
+                ),
+              )
             ],
           ),
       ],
